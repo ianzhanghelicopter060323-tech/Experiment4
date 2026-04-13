7a0df32b822003fb7fef74d7c36b59ad74a64c6a
Ian Zhang <ianzhanghelicopter060323@gmail.com>
2026-04-13 14:42:36 +0800

    new a pointer with Class

--- new.cpp
+++ new.cpp
@@ -3,6 +3,7 @@
 */
 
 #include <iostream>
+#define SIZE 10
 using namespace std;
 
 
@@ -11,9 +12,43 @@ class NewPrt
     public:
         NewPrt()
         {
-
+            cout<<"CONSTRUCT CALLED"<<endl;
+            pointer = new int[SIZE]; 
         }
         
+        ~NewPrt()
+        {
+            cout<<"DECONSTRUCT CALLED"<<endl;
+            delete [] pointer;
+            pointer = nullptr;
+        }
+
+
+        void prtPrint() const
+        {
+            for (int i=0; i<SIZE; i++)
+                cout<<pointer[i]<<" ";
+
+            cout<<'\n';
+        }
+
+        void setPointer(int value_input, int pos_input)
+        {
+            pointer[pos_input] = value_input;
+        }
     private:
-        int *pointer;
+        int *pointer; // int pointer. size 10
 };
+
+
+int main()
+{
+    NewPrt ptr;
+
+    // pointer envalued
+    for (int i=0; i<SIZE; i++)
+        ptr.setPointer(i+1, i);
+
+    ptr.prtPrint();
+    return 0;
+}
