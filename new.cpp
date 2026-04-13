410a5f775ccf03600c2c2b2375418efd190dc64d
Ian Zhang <ianzhanghelicopter060323@gmail.com>
2026-04-13 09:39:41 +0800

    add a new file: new.cpp(for memory allocate syntax)

---
+++ new.cpp
@@ -0,0 +1,19 @@
+/*
+声明一个int型指针，用new语句为其分配包含10个元素的地址空间，并赋值为1到10并打印；然后释放空间 
+*/
+
+#include <iostream>
+using namespace std;
+
+
+class NewPrt
+{
+    public:
+        NewPrt()
+        {
+
+        }
+        
+    private:
+        int *pointer;
+};
