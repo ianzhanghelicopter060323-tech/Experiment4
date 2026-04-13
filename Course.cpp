3376fc26b7c3d6b01a26b77bf927d7ccc2a522ee
Ian Zhang <ianzhanghelicopter060323@gmail.com>
2026-04-13 17:18:32 +0800

    Couse.cpp requirment

---
+++ Course.cpp
@@ -0,0 +1,9 @@
+/*
+在一个大学的选课系统中，包括两个类：CourseSchedule类和Course类，其关系为CourseSchedule类中的成员函数add和remove的参数是Course类的对象，courseSchedule类中要有一个成员变量用来存放多个Course，add和remove函数用来添加和删除一个coureschedule中的course，这里请自己思考用来存放多个Course的成员变量应该如何设计，参考上课讲过的关于停车场的例子，course类的设计请自己完成（这里的CourseSchedule类是一个课程列表有多个课程对象）
+
+提示：1、可以用数组存放（100）(本题里面显示50分）
+2、可以用动态分配的数组存放（120）(本题里面显示60分）
+3、可以用动态分配的链表存放（200）(本题里面显示100分）
+
+不同的实现方式将获得不同的上机分数 
+*/
